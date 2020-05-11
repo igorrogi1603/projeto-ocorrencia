58c0b336a0e37d2c77271c69ef5b18e9277e2327
Igor Santos <igor_rogi_16032000@hotmail.com>
2020-05-11 16:03:52 -0300

    Teste

---
+++ App/Views-cache/login.9cd50e92e96e96d5054d4ef81e55139c.rtpl.php
@@ -0,0 +1,82 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/res/site/dist/css/bootstrap4/bootstrap.min.css">
+
+    <link rel="stylesheet" href="/res/site/dist/css/css-login.css">
+
+    <title>SOM | Login</title>
+  </head>
+  <body>
+    
+    <div class="container-fluid esticado">
+      <div class="row esticado">
+        <div class="col-md-8 bg-image d-flex align-items-center">
+          <div class="bg-branco mx-auto d-block d-flex align-items-center fundo-img">
+            <img class="mx-auto" src="/res/site/dist/img/logo-azul.png" width="70">
+          </div>
+        </div>
+        
+        <div class="col-md-4 esticado bg-branco p-5">
+          <h1 class="mb-5 text-cor">Entrar</h1>
+
+          <form action="/login" method="post">
+            <div class="form-group">
+                <label class="text-normal text-dark">Usuário</label>
+                <input type="text" name="usernameUsuario" class="form-control" placeholder="Usuário" onkeyup="validarCaracter(this, 2)" required>
+            </div>
+            <div class="form-group">
+                <label class="text-normal text-dark">Senha</label>
+                <input type="password" name="senhaUsuario" class="form-control" placeholder="Senha" required>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                <?php if( $error != '' ){ ?>
+                <div class="alert alert-danger">
+                  <?php echo htmlspecialchars( $error, ENT_COMPAT, 'UTF-8', FALSE ); ?>
+                </div>
+                <?php } ?>
+              </div>
+            </div>
+
+            <p class="text-right esqueci"><a href="#">Esqueci a senha</a></p>
+            <button class="btn btn-block btn-cor arredondamento">Logar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="/res/site/dist/js/bootstrap4/jquery-3.3.1.slim.min.js"></script>
+    <script src="/res/site/dist/js/bootstrap4/popper.min.js"></script>
+    <script src="/res/site/dist/js/bootstrap4/bootstrap.min.js"></script>
+
+    <script>
+      function validarCaracter(valor, tipo)
+      {
+        switch (tipo) {
+          case 1:
+            string = valor.value.replace(/[^a-zA-Zà-úÀ-Ú ]/g,'');
+            valor.value = string;
+            break;
+          case 2:
+            string = valor.value.replace(/[^a-zA-Z0-9 ]/g,'');
+            valor.value = string;
+            break;
+          case 3:
+            string = valor.value.replace(/[^a-zA-Z0-9à-úÀ-Ú ]/g,'');
+            valor.value = string;
+            break;
+        }
+      }
+    </script>
+
+  </body>
+</html>
